8c547a1ce1b87228f7d31e87aa00e184cca5b122
Ahmad Saleh <dev.ahmadsaleh@gmail.com>
2022-05-15 23:35:56 +0300

    Time: 109 ms (53.34%), Space: 42.5 MB (70.31%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,18 +6,13 @@
 var twoSum = function(nums, target) {
    let  number = [];
    for ( let i = 0; i < nums.length -1 ; i++ ){
-            for (let j = i + 1 ; j < nums.length ; j++){
+       for (let j = i + 1 ; j < nums.length ; j++){
            if (nums[i] + nums[j] == target){
             number.push(i) ;
             number.push(j) ;
              break;
-         }
-            }
-};
-    
-        
-        
-         return number;  
-       
-            
-        }
+           }
+       }
+   };
+    return number;
+}
